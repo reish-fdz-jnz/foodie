8eb20faf28f69beb602fb7b3eada062ca3a31519
Reishell <152831865+reish-fdz-jnz@users.noreply.github.com>
2024-07-28 01:04:29 -0600

    app with register/login working

---
+++ Foodie.App/Foodie.Web/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Foodie.Web.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public UserRepository() 
+        { 
+        
+        }
+    }
+}
---
+++ Foodie.App/Foodie.Web/Services/UserService.cs
@@ -0,0 +1,17 @@
+﻿using Foodie.Web.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Foodie.Web.Services
+{
+    public class UserService
+    {
+        private IUserRepository userRepository;
+        public UserService(IUserRepository userRepository)
+        { 
+            this.userRepository = userRepository;
+        }
+    }
+}
