d4744c170e2a9c65473096e11b4ebb881f69058b
Reishell <152831865+reish-fdz-jnz@users.noreply.github.com>
2024-08-09 14:23:51 -0600

    Account code refactored

--- Foodie.App/Foodie.Web/Controllers/HomeController.cs
+++ Foodie.App/Foodie.Web/Controllers/HomeController.cs
@@ -71,18 +71,5 @@ namespace Foodie.Web.Controllers
 
             return RedirectToAction("Index","Home",filteredProducts);
         }
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
